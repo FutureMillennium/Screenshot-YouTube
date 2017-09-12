06968262c723538de5c89dfe44ed7a902c8a55a1
Zdeněk Gromnica <futuremillennium@gmail.com>
2017-09-12 12:49:18 +0200

    + rewrote to make it cleaner and work with current YouTube * changed name to “Screenshot YouTube” (avoid having a product name as the first word)

--- page.js
+++ page.js
@@ -1,61 +1,32 @@
-// Copyright (c) 2013 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
+function CaptureScreenshot() {
+	var title = document.querySelectorAll("h1.title")[0].innerHTML.trim();
 
-function screenCapture(){
-	//get the name of the video
-	var videoTitle = document.getElementsByClassName("watch-title")[0].innerHTML.trim();
-
-	var videoPlayer = document.getElementsByClassName("video-stream")[0];
+	var player = document.getElementsByClassName("video-stream")[0];
 	var canvas = document.createElement("canvas");
-	canvas.width = videoPlayer.videoWidth;
-	canvas.height = videoPlayer.videoHeight;
-	canvas.getContext('2d').drawImage(videoPlayer, 0, 0, canvas.width, canvas.height);
+	canvas.width = player.videoWidth;
+	canvas.height = player.videoHeight;
+	canvas.getContext('2d').drawImage(player, 0, 0, canvas.width, canvas.height);
 
-	//var img = document.createElement("img");
-	//img.src = canvas.toDataURL();
-	//document.body.prepend(img);
-	var imgdata = canvas.toDataURL('image/png');
-	//var newdata = imgdata.replace(/^data:image\/png/,'data:application/octet-stream');
-	//location.href = imgdata;
+	var dataImage = canvas.toDataURL('image/png');
 
-	var dummyAnchor = document.createElement("a");
-	dummyAnchor.href = imgdata;
-	dummyAnchor.download = videoTitle+"-screenshot.png";
-	dummyAnchor.click();
+	var downloadLink = document.createElement("a");
+	downloadLink.href = dataImage;
+	downloadLink.download = title + "-screenshot.png";
+	downloadLink.click();
 }
-/*
-chrome.tabs.onUpdated.addListener.addListener(
-  function(tabId, changeInfo, tab) {
-    window.console.log('updated from contentscript');
-  }
-);*/
 
-var button = document.createElement("button");
-button.className = "screenshotButton ytp-button";
-button.background = "none";
-button.border = "0px";
-button.style.width = "65px";
-button.innerHTML = "Capture";
-button.style.color = "white";
-button.style.cssFloat = "left";
-button.onclick=screenCapture;
-/*
-var img = document.createElement("img");
-img.src="https://cdn3.iconfinder.com/data/icons/mnml/screenshot.png";
-img.height="20px";
-//button.innerHTML = "hello";
-button.appendChild(img);
-*/
-//document.body.prepend(button);
-var interval = window.setInterval(function(){
-	var ytpControlsRight = document.getElementsByClassName("ytp-right-controls")[0];
-	if(ytpControlsRight && ytpControlsRight.innerHTML!=="Capture"){
-		ytpControlsRight.prepend(button);
-		clearInterval(interval);
-	}
-},2000);
-	
+var screenshotButton = document.createElement("button");
+screenshotButton.className = "screenshotButton ytp-button";
+screenshotButton.background = "none";
+screenshotButton.border = "0px";
+screenshotButton.style.width = "65px";
+screenshotButton.innerHTML = "Screenshot";
+screenshotButton.style.color = "white";
+screenshotButton.style.cssFloat = "left";
+screenshotButton.onclick = CaptureScreenshot;
 
-
+var ytpControlsRight = document.getElementsByClassName("ytp-right-controls")[0];
+if (ytpControlsRight) {
+	ytpControlsRight.prepend(screenshotButton);
+}
