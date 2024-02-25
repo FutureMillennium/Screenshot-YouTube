c73adb954788e7f34d1dee0d0fd8c3e0a6f33c71
unishigure <uni@seanut.app>
2024-02-25 15:09:02 +0900

    Remove Invalid ServiceWorker
    Invalid ServiceWorker by inactive
    https://developer.chrome.com/docs/extensions/develop/migrate/to-service-workers#keep-sw-alive

--- extension/background.js
+++
@@ -1,5 +0,0 @@
-chrome.webNavigation.onHistoryStateUpdated.addListener(function(data) {
-	chrome.tabs.get(data.tabId, function(tab) {
-		chrome.tabs.executeScript(data.tabId, {code: 'if (typeof AddScreenshotButton !== "undefined") { AddScreenshotButton(); }', runAt: 'document_start'});
-	});
-}, {url: [{hostSuffix: '.youtube.com'}]});
